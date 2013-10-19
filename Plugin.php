9cc9754de9e77b29978389707393ecee65838bfe
Arnaud Ligny <arnaud@ligny.org>
2013-10-20 01:10:37 +0200

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -1,34 +1,29 @@
 <?php
 namespace PHPoole;
+use PHPoole\Utils;
 
 /**
  * PHPoole plugin TwBootstrap
  */
 Class TwBootstrap extends Plugin
 {
-    const DEBUG = false;
-
     public function preInit($e)
     {
-        $this->trace(self::DEBUG, $e);
-        //
         $phpoole = $e->getTarget();
-        $phpoole->setMessage('TwBoostrap plugin available');
+        $phpoole->addMessage('TwBoostrap plugin available');
     }
 
     public function postInit($e)
     {
-        $this->trace(self::DEBUG, $e);
-        //
         $phpoole = $e->getTarget();
-        RecursiveCopy(
+        Utils\RecursiveCopy(
             __DIR__ . '/assets',
             $phpoole->getWebsitePath() . '/' . PHPoole::PHPOOLE_DIRNAME . '/' . PHPoole::ASSETS_DIRNAME
         );
-        RecursiveCopy(
+        Utils\RecursiveCopy(
             __DIR__ . '/layouts',
             $phpoole->getWebsitePath() . '/' . PHPoole::PHPOOLE_DIRNAME . '/' . PHPoole::LAYOUTS_DIRNAME
         );
-        $phpoole->setMessage('Twitter Boostrap layouts ans assets copied');
+        $phpoole->addMessage('Twitter Boostrap layouts ans assets copied');
     }
 }
